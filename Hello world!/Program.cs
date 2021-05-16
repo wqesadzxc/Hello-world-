3a9215ec1b4090c9182bd201156f175b0af673d4
wqesadzxc <84220765+wqesadzxc@users.noreply.github.com>
2021-05-16 13:59:42 +0500

    update output message

--- Hello world!/Program.cs
+++ Hello world!/Program.cs
@@ -6,7 +6,7 @@ namespace Hello_world_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
