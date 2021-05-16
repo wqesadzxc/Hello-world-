38c2bb00d667f51ca0e196d9d0ff1ef591838979
wqesadzxc <84220765+wqesadzxc@users.noreply.github.com>
2021-05-16 13:47:06 +0500

    Добавьте файлы проекта.

---
+++ Hello world!/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_world_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
